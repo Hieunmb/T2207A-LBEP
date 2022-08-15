2f0403dee642a3ca4ab1bb8eb69e6ad2e82b5c94
Hieunmb <zhuongdae@gmail.com>
2022-08-15 17:28:07 +0700

    bai tap

--- Assignment 6/bai 1.cpp
+++ Assignment 6/bai 1.cpp
@@ -2,17 +2,17 @@
 int main(){
 	int n;
 	int s,m;
+	s=m=0;
 	printf("Nhap tong so mang n=");
 	scanf("%d",&n);
-	s=m=0;
 	int ary[n];
 	for(int i=0;i<n;i++){
 		scanf("%d",&ary[i]);
 	}
 	for(int i=0;i<n;i++){
-		if(ary[i]%2==1);
+		if(ary[i]%2==1){
 		 s=s+ary[i];
-	     m=m+1;
+	     m=m+1;}
 	}
 	printf("Trung binh cong cac so le la %d",s/m);
 }
